cf6306f558c5c338c4c469e8de982902db6d222e
oceanbbbbbb <bhy71090@163.com>
2020-03-11 17:39:05 +0800

    init基本已实现

---
+++ src/main/java/com/ocean/swaggerdoc/SwaggerdocApplication.java
@@ -0,0 +1,13 @@
+package com.ocean.swaggerdoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerdocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwaggerdocApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ocean/swaggerdoc/SwaggerdocApplicationTests.java
@@ -0,0 +1,15 @@
+package com.ocean.swaggerdoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwaggerdocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+
+
+}
